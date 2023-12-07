c200ac364b46631412a2af0371067211a7284971
samrose <sam@floxdev.com>
2023-12-07 09:30:17 -0500

    feat: create care team and add user to group roles when user is created

--- trs/reentry/models.py
+++ trs/reentry/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class CareTeam(models.Model):
     name = models.CharField(max_length=100)
@@ -13,18 +15,22 @@ class ParoleOfficer(models.Model):
     care_teams = models.ManyToManyField(CareTeam, related_name="parole_officers", blank=True)
     organization = models.CharField(max_length=100)
 
-    def add_to_group(self):
-        group, created = Group.objects.get_or_create(name='Parole Officer')
-        self.user.groups.add(group)
+@receiver(post_save, sender=ParoleOfficer)
+def create_parole_officer_group(sender, instance, created, **kwargs):
+    if created:
+        group, created = Group.objects.get_or_create(name='Parole Officer Role')
+        instance.user.groups.add(group)
 
 class Mentor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     care_teams = models.ManyToManyField(CareTeam, related_name="mentors", blank=True)
     organization = models.CharField(max_length=100)
 
-    def add_to_group(self):
-        group, created = Group.objects.get_or_create(name='Mentor')
-        self.user.groups.add(group)
+@receiver(post_save, sender=Mentor)
+def create_mentor_group(sender, instance, created, **kwargs):
+    if created:
+        group, created = Group.objects.get_or_create(name='Mentor Role')
+        instance.user.groups.add(group)
 
 class ReturningCitizen(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -32,9 +38,6 @@ class ReturningCitizen(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    def add_to_group(self):
-        group, created = Group.objects.get_or_create(name='Returning Citizen')
-        self.user.groups.add(group)
 
     def save(self, *args, **kwargs):
         # If a care team does not exist, create one and associate it with the user
@@ -42,3 +45,9 @@ class ReturningCitizen(models.Model):
             care_team = CareTeam.objects.create(name=f"Care Team for {self.first_name} {self.last_name}")
             self.care_team = care_team
         super().save(*args, **kwargs)
+
+@receiver(post_save, sender=ReturningCitizen)
+def create_returning_citizen_group(sender, instance, created, **kwargs):
+    if created:
+        group, created = Group.objects.get_or_create(name='Returning Citizen Role')
+        instance.user.groups.add(group)
