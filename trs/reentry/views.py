d80740f88843e9a418c3e3fb361523d4d9497719
samrose <sam@floxdev.com>
2024-01-23 11:17:45 -0500

    feat: showing a mentor their connected care teams

--- trs/reentry/views.py
+++ trs/reentry/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseForbidden
+from django.template.loader import render_to_string
+from django.http import HttpResponse,HttpResponseForbidden
 from django.contrib.auth.decorators import user_passes_test, login_required
 from reentry.models import (
     ReturningCitizen,
@@ -45,6 +46,46 @@ def is_mentor(user):
 mentor_required = user_passes_test(is_mentor, login_url="home")
 
 
+
+def get_mentor_care_teams(user):
+    # Check if the user is a Mentor
+    try:
+        mentor = Mentor.objects.get(user=user)
+    except Mentor.DoesNotExist:
+        # Handle the case when the user is not a Mentor
+        return {'error': 'Not a Mentor'}
+    #print(mentor)
+    # Retrieve the associated CareTeams
+    care_teams = mentor.care_teams.all()
+
+    # Convert CareTeams data to a format suitable for JSON
+    care_teams_data = [{'name': team.name} for team in care_teams]
+    #print(care_teams_data)
+    # Return the CareTeams data as a dictionary
+    return {'care_teams': care_teams_data}
+
+
+# @login_required
+# @user_passes_test(lambda u: get_model_for_group(u) is not None, login_url=None)
+# def home(request):
+#     group_mapping = get_model_for_group(request.user)
+
+#     try:
+#         model_instance = group_mapping["model"].objects.get(user=request.user)
+#         # template_name = group_mapping['template']
+
+#         # Create a variable with the model name to pass to the template
+#         model_name = group_mapping["model"].__name__.lower()
+#         return render(
+#             request,
+#             "home.html",
+#             {"model_instance": model_instance, "model_name": model_name},
+#         )
+#     except group_mapping["model"].DoesNotExist:
+#         return HttpResponseForbidden(
+#             f"You do not have a {group_mapping['model'].__name__} profile."
+#         )
+
 @login_required
 @user_passes_test(lambda u: get_model_for_group(u) is not None, login_url=None)
 def home(request):
@@ -52,10 +93,18 @@ def home(request):
 
     try:
         model_instance = group_mapping["model"].objects.get(user=request.user)
-        # template_name = group_mapping['template']
-
-        # Create a variable with the model name to pass to the template
         model_name = group_mapping["model"].__name__.lower()
+
+        if model_name == 'mentor':
+            # Call the function to get mentor care teams data
+            mentor_care_teams_data = get_mentor_care_teams(request.user)
+
+            return render(
+                request,
+                "home.html",
+                {"model_instance": model_instance, "model_name": model_name, "mentor_care_teams_data": mentor_care_teams_data},
+            )
+
         return render(
             request,
             "home.html",
@@ -65,8 +114,7 @@ def home(request):
         return HttpResponseForbidden(
             f"You do not have a {group_mapping['model'].__name__} profile."
         )
-
-
+        
 @login_required
 @mentor_required
 def display_questionnaire(request, questionnaire_id):
